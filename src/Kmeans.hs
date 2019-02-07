3cb527546c5819ed2fbdc8f00f098cc76ba6f98b
Ethan Riley <riley787cmplex@gmail.com>
2019-02-07 12:43:14 +0000

    fix all errors

--- src/Kmeans.hs
+++ src/Kmeans.hs
@@ -6,11 +6,12 @@ where
 
 import System.Random
 import Data.Vector (minimum)
+import Data.Word (Word8)
+import qualified Data.Vector.Unboxed as V
 
-import Permutation (permutate)
-
-import Data.Array.Repa (traverse, foldP, slice, All, Any, Array, DIM1, DIM2, U, D, Z (..), (:.)(..), (!), (-^)(..))
+import Data.Array.Repa (foldS, slice, All, Any, Array, DIM1, DIM2, U, D, Z (..), (:.)(..))
 import qualified Data.Array.Repa as R
+import qualified Data.Array.Repa.Shape as R.Shape
 
 kmeans :: Array U DIM2 RGB8 -> Int -> IO [Array U DIM2 RGB8]
 kmeans pixels clusters = do
@@ -22,51 +23,57 @@ iterateKmeans centroids pixels
     | newCentroids == centroids = centroids
     | otherwise = iterateKmeans newCentroids pixels
     where 
-        newCentroids = map mean $ iteration centoids pixels 
+        newCentroids = map mean $ iteration centroids pixels 
 
 guessClusters :: Int -> Array U DIM2 RGB8 -> IO [Array U DIM2 RGB8]
 guessClusters clusters pixels = do
     gen <- getStdGen
-    let rs = map (\s -> take clusters $ randomRs (0, s) gen) 
-        $ R.Shape.toList $ extent pixels
-    let colours = map (\(x,y) -> x ! (Z :. x :. y)) $ zip rs[0] rs[1]
+    let rs = map (\s -> take clusters $ randomRs (0, s) gen) $ R.Shape.listOfShape $ R.extent pixels
+    let colours = map (\(x,y) -> pixels R.! (Z :. x :. y)) $ zip (rs !! 0) (rs !! 1)
     return $ iteration colours pixels 
     
 iteration :: [RGB8] -> Array U DIM2 RGB8 -> [Array U DIM2 RGB8]
 iteration centroids pixels = 
-        R.map (\(pixel dist) -> if dist < midpoint then pixel else zeroPixel) 
-            $ map (R.zipWith (,) pixels) $ pairwiseDistanceMin centroids pixels
+    map (R.computeUnboxedS . R.map (\(pixel, (mid, dist)) -> if dist < mid then pixel else zeroPixel))
+        (map ( R.computeUnboxedS . R.zipWith (,) pixels . R.zipWith (,) midDist) 
+            $ pairwiseDistance centroids pixels)
     where 
-        midpoint = meanList centroids
+        midDist = midpointDistance centroids pixels
    
 meanList :: [RGB8] -> RGB8
 meanList xs = 
-        (fst c / size xs, snd c / size xs, thd c / size xs)
+        (first c / s, secnd c / s, thd c / s)
     where 
-        c = foldr1 (\acc new -> (fst acc + fst new, 
-                snd acc + snd new, thd acc + thd new)) xs 
+        c = foldr1 (\acc new -> (first acc + first new, 
+                secnd acc + secnd new, thd acc + thd new)) xs 
+        s = fromIntegral $ length xs
     
-pairwiseDistanceMin :: [RGB8] -> Array U DIM2 RGB8 -> [Array U DIM2 RGB8]
-pairwiseDistanceMin centroids pixels = 
-    map (\c -> R.map (distance c)) centroids
+pairwiseDistance :: [RGB8] -> Array U DIM2 RGB8 -> [Array U DIM2 Word8]
+pairwiseDistance centroids pixels = 
+    map (\c -> R.computeUnboxedS $ R.map (distance c) pixels) centroids
 
-mean :: (R.Shape sh) => Array U sh RGB8 -> RGB8
+midpointDistance :: [RGB8] -> Array U DIM2 RGB8 -> Array U DIM2 Word8
+midpointDistance centroids pixels = 
+    R.computeUnboxedS $ R.map (distance m) pixels 
+    where m = meanList centroids 
+
+mean :: Array U DIM2 RGB8 -> RGB8
 mean arr = 
-        (fst c / s, snd c / s, thd c / s)
+        (first c / s , secnd c / s, thd c / s)
     where
-       c = (foldP (\a n -> (fst a + fst n, snd a + snd n, thd a + thd n)) 
-                zeroPixel arr) 
-       s = R.size $ extent arr 
+       c = (V.foldl (\a n -> (first a + first n, secnd a + secnd n, thd a + thd n)) 
+                zeroPixel $ R.toUnboxed arr) 
+       s = fromIntegral $ R.size $ R.extent arr 
 
 zeroPixel :: RGB8
-zeroPixel  = (0::Pixel8, 0::Pixel8, 0::Pixel8)
+zeroPixel = (0::Word8, 0::Word8, 0::Word8)
 
-distance :: RGB8 -> RGB8 -> Double 
+distance :: RGB8 -> RGB8 -> Word8
 distance op np = sqrt 
-    $ foldr (\acc fn -> acc + ((fn op) - (fn np)) ** 2) (0::Double) [fst, snd, thd]
+    $ foldr (\fn acc -> acc + ((fn op) - (fn np)) ** 2) (0) [first, secnd, thd]
 
-fst (x, _, _) = x
-snd (_, x, _) = x
+first (x, _, _) = x
+secnd (_, x, _) = x
 thd (_, _, x) = x 
 
-type RGB8 = (Pixel8, Pixel8, Pixel8)
+type RGB8 = (Word8, Word8, Word8)
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,34 @@
 module Main where
 
+import Kmeans
+
+import Codec.Picture
+
+-- | Produce delayed Repa array from image with true color pixels.
+fromImage :: Image PixelRGB8 -> Array D DIM2 RGB8
+fromImage img@Image {..} =
+  R.fromFunction
+    (Z :. imageWidth :. imageHeight)
+    (\(Z :. x :. y) ->
+       let (PixelRGB8 r g b) = pixelAt img x y
+       in (r, g, b))
+
+-- | Get image with true color pixels from manifest Repa array.
+toImage :: Array U DIM2 RGB8 -> Image PixelRGB8
+toImage a = generateImage gen width height
+  where
+    Z :. width :. height = R.extent a
+    gen x y =
+      let (r,g,b) = a ! (Z :. x :. y)
+      in PixelRGB8 r g b
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    [path, _] <- getArgs
+    eimg <- readImage path 
+    case eimg of 
+        Left err <- putStrLn "could not open image"
+        Right (ImageRGB8 img) -> do
+            clusters <- kmeans (fromImage img) 16
+            map (\(c, i) -> savePngImage (show i) $ toImage c) $ zip clusters [1..]
+            putStrLn "operation complete"
